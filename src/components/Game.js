666748fff3f1a63de9ac9a3205ee207c04fd7182
Tristan <tristankjeld@gmail.com>
2023-07-17 21:36:17 +0200

    add score to game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import $ from 'jquery'
 
-import PlayAgain from './PlayAgain'
+import { Link } from 'react-router-dom'
 
 function Game () {
   const [image, setImage] = useState('')
-  const [item, setItem] = useState('')
-  const [gamePlayed, setGamePlayed] = useState(false)
+  const [answer, setAnswer] = useState('')
+  const [lost, setLost] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const [points, setPoints] = useState(0)
 
   // ajax request to get the image for the game
   const getGameImage = () => {
@@ -28,46 +30,65 @@ function Game () {
     }).done(function (json) {
     }).then(json => {
       setImage(json.collection.items[randomNumber].links[0].href)
-      setItem(randomSearchItem)
+      setAnswer(randomSearchItem)
     })
-  }
-
-  // the game choices are rendered
-  const playGame = () => {
-    const spaceWords = ['moon', 'earth', 'jupiter', 'saturn', 'pluto', 'mars', 'venus']
-    return spaceWords.map(word =>
-        <div className="guessing">
-          <button onClick={ e => guessChoice(e)} id={word}>{word}</button>
-        </div>
-    )
   }
 
   // the player chooses one item and this function determines if it's a win
   const guessChoice = (e) => {
-    setGamePlayed(true)
-
-    if (item === e.target.id) {
-      $('.namegamebutton').html("You're Right!")
+    if (answer !== e.target.id) {
+      setLost(true)
     } else {
-      $('.namegamebutton').html('Wrong, Try Again. Correct Answer: ' + item)
+      setPoints(points + 1)
     }
   }
 
   useEffect(() => {
-    getGameImage()
-  })
+    if (!lost) { getGameImage() }
+  }, [points, lost])
 
-  const renderGame = () => {
-    return <div>{playGame()}</div>
+  const youLost = () => {
+  return (
+    <div className="playagainbutton">
+      <p>Incorrect! Correct answer: {answer}</p>
+      <button onClick={() => { document.location.reload(true) }}>Play again!</button>
+      <Link to="/"><button className="back">Go Back</button></Link>
+    </div>
+  )
+}
+
+  const game = () => {
+    const spaceWords = ['moon', 'earth', 'jupiter', 'saturn', 'pluto', 'mars', 'venus']
+
+    return (
+      <div>
+        {spaceWords.map(
+          (word) => {
+            return <button id={word} disabled={loading} onClick={e => { guessChoice(e) }}>{word}</button>
+          }
+        )}
+      </div>
+    )
   }
 
   return (
     <div className="namegame">
-      <div className="titlegame">Guess which one is associated with this image:</div>
-      <img src={image} alt="" id="namegameimage" />
-      {renderGame()}
-      {gamePlayed ? <PlayAgain /> : null }
-    </div>
+      <h1>Score: {points}</h1>
+    {
+      lost ? (youLost()) : (
+        <>
+        <h2>Guess which one is associated with this image:</h2>
+        <div style={{display: loading ? "block" : "none"}}>
+           Loading...
+        </div>
+        <div style={{display: loading ? "none" : "block"}}>
+          <img src={image} alt="" id="namegameimage" onLoad={() => {setLoading(false)}} />
+        </div>
+        {game()}
+        </>
+        )
+    }
+  </div>
   )
 }
 
